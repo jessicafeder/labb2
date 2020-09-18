0f5913c942f4062fd26998736d213349588ccbd5
jessicafeder <56555778+jessicafeder@users.noreply.github.com>
2020-09-18 18:14:46 +0200

    Add files via upload

---
+++ Labb2/src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void FileCrawler(String name, File file) {
+
+
+        try {
+            Scanner search = new Scanner(file);
+            while (search.hasNextLine()) {
+                if (name.equals(search.next())){
+                    System.out.println(file.getAbsolutePath());
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void WhereIsIt(String name, File file){
+
+        if (file.isDirectory()){
+            File[] folderContents = file.listFiles();
+            for (int i = 0; i < folderContents.length; i++){
+                File f = folderContents[i];
+                WhereIsIt(name, f);
+            }
+
+        } else{
+            FileCrawler(name, file);
+        }
+
+    }
+
+    public static String InputName(){
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        return name;
+    }
+
+
+    public static void main(String[] args) {
+        File file = new File("C:\\DEV\\Labb2");
+        System.out.println("Skriv in ett sökord: ");
+        String name = InputName();
+        WhereIsIt(name, file);
+        //System.out.println(name);
+        //FileCrawler(name, file);
+
+
+
+    }
+}
