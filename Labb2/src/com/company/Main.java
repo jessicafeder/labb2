6bb4249f70ec31a38c9c7ddca3c0770d7ef30cd8
jessicafeder <56555778+jessicafeder@users.noreply.github.com>
2020-09-21 21:59:09 +0200

    Update Main.java
    Ytterligare uppdateringar

--- Labb2/src/com/company/Main.java
+++ Labb2/src/com/company/Main.java
@@ -10,37 +10,39 @@ public class Main {
             while (search.hasNext()) {
                 if (name.equalsIgnoreCase(search.next())){
                     System.out.println(file.getAbsolutePath());
+                    break;
                 }
+
             }
         } catch (Exception e) {
-            System.err.println("Filen hittades inte:");
+            System.err.println("Filen hittades inte: " + file.getAbsolutePath());
             e.printStackTrace();
         }
     }
 
-    public static void WhereIsIt(String name, File file){
+    public static void whereIsIt(String name, File file){
 
         if (file.isDirectory()){
             File[] folderContents = file.listFiles();
             for (int i = 0; i < folderContents.length; i++){
                 File f = folderContents[i];
-                WhereIsIt(name, f);
+                whereIsIt(name, f);
             }
         } else{
             searchForIt(name, file);
         }
     }
 
-    public static String InputName(){
+    public static String inputName(){
         Scanner input = new Scanner(System.in);
         String name = input.next();
         return name;
     }
 
     public static void main(String[] args) {
-        File file = new File("C:\\DEV\\Labb2");
+        File file = new File("C:\\DEV\\Labbb2");
         System.out.println("Skriv in ett sökord: ");
-        String name = InputName();
-        WhereIsIt(name, file);
+        String name = inputName();
+        whereIsIt(name, file);
     }
 }
